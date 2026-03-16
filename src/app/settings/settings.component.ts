ec097d0712129281f5da3519be37664257568194
jose906 <jrestenssoro@hotmail.com>
2026-03-16 18:46:32 -0400

    mejora settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -46,6 +46,7 @@ export class SettingsComponent implements OnInit {
       next: (res) => {
         const ids = (res?.rows || []).map((r: any) => String(r.tweetuser_id));
         this.followingUsers = new Set(ids);
+        console.log(res)
       },
       error: (e) => {
         console.error('Error cargando seguidos', e);
